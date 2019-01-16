e6a7bbaef8c23907c8496f9688229ac2616e64c1
Yegor Kozlovskiy <basterfly@gmail.com>
2019-01-16 18:50:04 +0200

    Skutarenko 14 NSString HOMEWORK PUPIL

--- Scooter14NSString/Scooter14NSString/AppDelegate.m
+++ Scooter14NSString/Scooter14NSString/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "YKStringHW.h"
 
 @interface AppDelegate ()
 
@@ -17,92 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    
-    NSString *string1 = @"String1";
-    NSString *string2 = @"String2";
-    
-    NSArray *strings = [NSArray arrayWithObjects:string1, string2, nil];
-    for (NSString *str in strings) {
-        if ([str isEqualToString:string2]) {
-            NSLog(@"index = %lu", (unsigned long)[strings indexOfObject:str]); //ALL Objects will to compare or isEqual
-            // and simple values we can compare like ==
-        }
-    }
-    
-    string1 = @"Hello, World!";
-    NSLog(@"%@", string1);
-    NSRange range = [string1 rangeOfString:@"World"];
-    NSLog(@"range = %@", NSStringFromRange(range));
-//
-    range = [string1 rangeOfString:@"world"];
-    if (range.location != NSNotFound) {
-        NSLog(@"range = %@", NSStringFromRange(range));
-    } else {
-        NSLog(@"not found");
-    }
-    
-//
-//    range = [string1 rangeOfString:@"world" options:NSCaseInsensitiveSearch];
-    range = [string1 rangeOfString:@"world" options:NSCaseInsensitiveSearch | NSBackwardsSearch];
-    if (range.location != NSNotFound) {
-        NSLog(@"range = %@", NSStringFromRange(range));
-    } else {
-        NSLog(@"not found");
-    }
-    
-    string2 = [string1 substringFromIndex:5];
-    NSLog(@"%@", string2);
-    string2 = [string1 substringToIndex:5];
-    NSLog(@"%@", string2);
-    string2 = [string1 substringWithRange:NSMakeRange (5, 7)];
-    NSLog(@"%@", string2);
-
-//
-    NSRange searchRange = NSMakeRange(0, [string1 length]);
-    NSUInteger counter = 0;
-    while (YES) {
-        NSRange range = [string1 rangeOfString:@"l" options:0 range:searchRange];
-        if (range.location != NSNotFound) {
-            NSUInteger index = range.location + range.length;
-            searchRange.location = index;
-            searchRange.length = [string1 length] - index;
-            NSLog(@"%@", NSStringFromRange(range));
-            counter++;
-        } else {
-            break;
-        }
-    }
-    
-    NSLog(@"Counter = %lu", counter);
-    
-//
-    string2 = [string1 stringByReplacingOccurrencesOfString:@"l" withString:@"L"];
-    NSLog(@"%@", string2);
-    
-    string2 = [string1 uppercaseString];
-    NSLog(@"%@", string2);
-    
-    string2 = [string1 lowercaseString];
-    NSLog(@"%@", string2);
-    
-    string2 = [string2 capitalizedString];
-    NSLog(@"%@", string2);
-    
-    NSLog(@"%d", [@"512" intValue]);
-    
-    string2 = [string1 stringByReplacingOccurrencesOfString:@"," withString:@" "];
-    string2 = [string2 stringByReplacingOccurrencesOfString:@"!" withString:@" "];
-    NSLog(@"%@", string2);
-    
-    strings = [string1 componentsSeparatedByString:@" "];
-    NSLog(@"%@", strings);
-    
-    string2 = [strings componentsJoinedByString:@"_"];
-    NSLog(@"%@", string2);
-    
-    // рассмотреть регулярные выражения!
-    
-    NSLog(@"%@", [string1 stringByAppendingString:string2]);
+    YKStringHW *hw = [YKStringHW new];
+    [hw stringHomework];
     
     return YES;
 }
---
+++ Scooter14NSString/Scooter14NSString/HomeWork/YKStringHW.h
@@ -0,0 +1,15 @@
+//
+//  YKStringHW.h
+//  Scooter14NSString
+//
+//  Created by Egor Kozlovskiy on 1/15/19.
+//  Copyright © 2019 Yegor Kozlovskiy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YKStringHW : NSObject
+
+-(void)stringHomework;
+
+@end
